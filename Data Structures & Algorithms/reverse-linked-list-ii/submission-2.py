ae51f8e81ef695167c9f3a5c34645702ddf912e0
The_Adel <richmondbawah61@gmail.com>
2026-06-06 10:28:42 -0400

    Add: reverse-linked-list-ii - submission-2

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-2.py
@@ -0,0 +1,56 @@
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        curr = head
+        i = 1
+        j =1
+        before =curr
+        after = None
+    
+        if left == right:
+            return head
+
+        while i < left or j<right and curr:
+            if i <left:
+                before= curr
+                curr = curr.next
+                i+=1
+                j+=1
+            
+            if j <right and i == left:
+                curr = curr.next
+                j+=1
+        
+        if not curr:
+            after= None
+        else:
+            after = curr.next
+        
+        prev = after
+        if i == 1:
+            curr = before
+        else:
+            curr = before.next
+
+        while curr != after:
+            nxt = curr.next
+            curr.next = prev
+
+            prev = curr
+            curr= nxt
+        
+        if i !=1:
+            before.next = prev
+            return head
+        else:
+            return prev
+
+
+
+
+        
+    
+        
+
+        
+
